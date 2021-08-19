4d060ff025650904151cf90c04b1ce028f933d0f
rajashekhar <rajashekharpasikanti@gmail.com>
2021-08-19 12:04:04 +0530

    two numbers are added

--- Addingtwonumbes.java
+++ Addingtwonumbes.java
@@ -8,9 +8,13 @@ public class Addingtwonumbes {
 	public static void main(String[] args) {
 		System.out.println("welcom adding two numbers");
 		Scanner scan = new Scanner(System.in);
+		System.out.println("enter a and b");
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		System.out.println(a +" "+b);
+		int c=a+b;
+		
+		System.out.println("answer " + c);
 	}
 
 }
