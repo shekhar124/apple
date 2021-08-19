e222e9884d6b844229151643731773233fd59dc5
rajashekhar <rajashekharpasikanti@gmail.com>
2021-08-19 11:58:21 +0530

    taken ip from user

--- Addingtwonumbes.java
+++ Addingtwonumbes.java
@@ -1,10 +1,16 @@
+
 package com.add.numbers;
+
+import java.util.Scanner;
 
 public class Addingtwonumbes {
 
 	public static void main(String[] args) {
 		System.out.println("welcom adding two numbers");
-
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		System.out.println(a +" "+b);
 	}
 
 }
